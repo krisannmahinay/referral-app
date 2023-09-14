d034f03666462d2bf6494ab6b98704df730dd57b
Mark Loreto <markyctrigger27@gmail.com>
2023-09-14 10:27:03 +0800

    this the modified code

--- process_form.php
+++ process_form.php
@@ -1,47 +1,30 @@
 <?php
-// Set the database connection details
-$servername = "localhost"; // Replace with your MySQL server name or IP address
-$username = "root"; // Replace with your MySQL username
-$password = ""; // Replace with your MySQL password
-$database = "referral_data"; // Replace with your database name
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "referral_app_db";
 
-// Connect to the database
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $dbname);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// PHP form submission handling code
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Get the form inputs from the $_POST superglobal
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $comment = $_POST['comment'];
-    $service1 = isset($_POST['service1']) ? $_POST['service1'] : '';
-    $service2 = isset($_POST['service2']) ? $_POST['service2'] : '';
+$first_name = mysqli_real_escape_string($conn, $_POST['first_name']);
+$last_name = mysqli_real_escape_string($conn, $_POST['last_name']);
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+$phone_number = mysqli_real_escape_string($conn, $_POST['phone_number']);
+$services_choices = implode(', ', $_POST['services_choices']);
+$message = mysqli_real_escape_string($conn, $_POST['message']);
 
-    // Insert the form data into a table
-    $sql = "INSERT INTO referrals (email, phone, first_name, last_name, comment, service1, service2) VALUES (?, ?, ?, ?, ?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    if (!$stmt) {
-        die("Error: " . $conn->error); // Display error message
-    }
+$sql = "INSERT INTO users (first_name, last_name, email, phone_number, services_choices, message) 
+        VALUES ('$first_name', '$last_name', '$email', '$phone_number', '$services_choices', '$message')";
 
-    $stmt->bind_param("sssssss", $email, $phone, $firstname, $lastname, $comment, $service1, $service2);
-    $result = $stmt->execute();
-    if (!$result) {
-        die("Error: " . $stmt->error); // Display error message
-    }
-
-    $stmt->close();
+if ($conn->query($sql) === TRUE) {
+    echo "Data inserted successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-// Close the database connection
 $conn->close();
-
-// Send a response back to the JavaScript code
-$response = array('status' => 'success', 'message' => 'Form submitted successfully');
-echo json_encode($response);
 ?>
--- script.js
+++ script.js
@@ -1,51 +1,127 @@
-// Function to validate email format
-function validateEmail(email) {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const showFormButton = document.getElementById('showFormButton');
+    const referralFormContainer = document.getElementById('referralFormContainer');
 
-// Add event listeners to handle button clicks
-smsButton.addEventListener('click', function () {
-  shareViaSMS();
+    showFormButton.addEventListener('click', function () {
+      // Toggle the display style of the referralFormContainer
+      if (referralFormContainer.style.display === 'none' || referralFormContainer.style.display === '') {
+        referralFormContainer.style.display = 'block';
+      } else {
+        referralFormContainer.style.display = 'none';
+      }
+    });
+  });
+document.addEventListener("DOMContentLoaded", function() {
+    const showFormButton = document.getElementById('showFormButton');
+    const referralForm = document.getElementById('referralForm');
+
+    showFormButton.addEventListener('click', function () {
+      referralForm.style.display = 'block';
+    });
+  });
+document.addEventListener("DOMContentLoaded", function() {
+    // Your existing code for sharing buttons
+    const floatingButtons = document.querySelectorAll(".sticky-button-container a");
+
+    // Add event listeners to handle button clicks
+    floatingButtons.forEach(button => {
+        button.addEventListener("click", function(event) {
+            event.preventDefault(); // Prevent the default link behavior
+            
+            const shareType = button.getAttribute("data-share");
+            
+            if (shareType === "facebook") {
+                shareOnFacebook();
+            } else if (shareType === "sms") {
+                shareViaSMS();
+            } else if (shareType === "instagram") {
+                shareOnInstagram();
+            } else if (shareType === "twitter") {
+                shareOnTwitter();
+            }
+        });
+    });
+
+    function shareOnFacebook() {
+        // Get the current URL of the web page
+        const currentUrl = window.location.href;
+    
+        // Construct the Facebook share URL with the current page's URL
+        const facebookUrl = `https://www.facebook.com/sharer.php?u=${encodeURIComponent(currentUrl)}`;
+        
+        // Open the Facebook share link in a new window
+        window.open(facebookUrl, '_blank', 'width=600,height=400');
+    }
+    
+    function shareOnInstagram() {
+        // Get the current URL of the web page
+        const currentUrl = window.location.href;
+    
+        // Construct the Instagram share URL with the current page's URL
+        const instagramUrl = `https://www.instagram.com/?url=${encodeURIComponent(currentUrl)}`;
+        
+        // Open the Instagram share link in a new window
+        window.open(instagramUrl, '_blank', 'width=600,height=400');
+    }
+    
+    function shareOnTwitter() {
+        // Get the current URL of the web page
+        const currentUrl = window.location.href;
+    
+        // Construct the Twitter share URL with the current page's URL and a default text
+        const twitterUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(currentUrl)}&text=Check out this referral link`;
+        
+        // Open the Twitter share link in a new window
+        window.open(twitterUrl, '_blank', 'width=600,height=400');
+    }
+    
+    
+    function shareViaSMS() {
+        // Prompt the user to enter the recipient's phone number
+        const phoneNumber = prompt('Enter recipient\'s phone number:');
+        
+        if (phoneNumber === null) {
+            // User clicked Cancel, do nothing
+            return;
+        }
+        
+        // Prompt the user to enter the message
+        const message = prompt('Enter your message:');
+        
+        if (message === null) {
+            // User clicked Cancel, do nothing
+            return;
+        }
+        
+        // Construct the SMS URL
+        const smsUrl = `sms:${phoneNumber}?body=${encodeURIComponent(message)}`;
+        
+        // Open the SMS link in a new window or send an SMS from a mobile device
+        window.open(smsUrl);
+    }
+    
+    
+
+
+    // Add event listeners to handle button clicks for email validation
+    const smsButton = document.querySelector('#smsButton');
+    const fbButton = document.querySelector('#fbButton');
+    const instaButton = document.querySelector('#instaButton');
+    const twtButton = document.querySelector('#twtButton');
+
+    smsButton.addEventListener('click', function () {
+        shareViaSMS();
+    });
+
+    fbButton.addEventListener('click', function () {
+        shareOnFacebook();
+    });
+
+    instaButton.addEventListener('click', function () {
+        shareOnInstagram();
+    });
+
+    twtButton.addEventListener('click', function () {
+        shareOnTwitter();
+    });
 });
-
-fbButton.addEventListener('click', function () {
-  shareOnFacebook();
-});
-
-instaButton.addEventListener('click', function () {
-  shareOnInstagram();
-});
-
-twtButton.addEventListener('click', function () {
-  shareOnTwitter();
-});
-
-
-function shareViaSMS() {
-  // Get the phone number of the person you want to refer.
-  phone_number = input("Enter the phone number of the person you want to refer: ");
-
-  // Get the text of the referral message.
-  referral_message = input("Enter the text of your referral message: ");
-
-  // Send the SMS message.
-  send_sms(phone_number, referral_message);
-}
-
-function shareOnFacebook() {
-  const referralUrl = window.location.href;
-  const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(referralUrl)}`;
-  window.open(shareUrl, '_blank', 'width=600,height=400');
-}
-
-function shareOnInstagram() {
-  // Replace this with your logic to share the app via Instagram
-}
-
-function shareOnTwitter() {
-  const referralUrl = window.location.href;
-  const tweetText = `Check out this website: ${referralUrl}`;
-  const shareUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`;
-  window.open(shareUrl);
-}
